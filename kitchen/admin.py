0f4556215cf1a4ae0373217450a82917c421e63e
Daniel Joseph <djoseph@envisagebr.com>
2025-09-28 11:18:23 +0300

    Add models: DishType, Cook, Dish (+optional Ingredient) and admin registrations

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import DishType, Cook, Dish, Ingredient
 
-# Register your models here.
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "years_of_experience")
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "dish_type", "price")
+    list_filter = ("dish_type",)
+    search_fields = ("name",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "unit")
+    search_fields = ("name",)
